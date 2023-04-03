c455c9d67ebe175483592032aa0c1f1cedc4f247
Jose Arnel <jonel.manipon@gmail.com>
2023-04-03 15:51:36 -0400

    capstone

---
+++ Capstone.R
@@ -0,0 +1,10 @@
+install.packages("tidyverse")
+install.packages("skimr")
+library(tidyverse)
+library(skimr)
+
+stocks_data <- read.csv2("/Users/josearnel/Documents/Coding_Den/Capstone/AMZN.csv", sep = ",",  fileEncoding="utf-16")
+
+
+
+
