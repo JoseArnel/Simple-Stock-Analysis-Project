348048865fe44b7d6c0938afce1e1530715586da
Jose Arnel <jonel.manipon@gmail.com>
2023-04-11 00:24:38 -0400

    pandas

--- analyze.py
+++ analyze.py
@@ -1,9 +1,28 @@
 import csv
+import pandas as pd
+from matplotlib import pyplot as plt
 
 def analyze():
     with open('AMZN.csv', newline='') as file:
-        csvreader = csv.DictReader(file)
+        csvreader = pd.read_csv(file)
+        ma = 0
         for row in csvreader:
-            print(row['Adj Close'])
+            print(int(float(row[0:])))
+
+
+# def analyze1():
+#     with open('analyze.csv', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerow(["MovingAverage"])
+#         with open('AMZN.csv', newline='') as file1:
+#             ma = 0
+#             csvreader = csv.DictReader(file1):
+#             for row in csvreader:
+#                 ma += row
+
+def read():
+    data = pd.read_csv('AMZN.csv')
+    plt.plot(data.AdjClose/100)
+    plt.show()
 
 analyze()
