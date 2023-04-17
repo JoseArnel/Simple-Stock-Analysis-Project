afcb0590118bccd143ceabce61d827c18765fabc
Jose Arnel <jonel.manipon@gmail.com>
2023-04-17 13:49:28 -0400

    play

--- analyze.py
+++ analyze.py
@@ -6,26 +6,23 @@ from matplotlib import pyplot as plt
 # How to sum a row in Pandas
 # indicies i, i+4
 
-
 def analyze():
-    with open('AMZN.csv', newline='') as file:
-        csvreader = pd.read_csv(file)
+    df = pd.read_csv('DataSets/AMZN.csv', skipinitialspace=True)
+    print(df.AdjClose)
+
+    # with open('DataSets/AMZN.csv', newline='') as file:
+    #     read = csv.reader(file)
+    #     for row in read():
+    #         print(row[0])
+
+# Simple sum
+def sum():
+    with open('DataSets/AMZN.csv') as file:
+        headerline = next(file)
         ma = 0
-        for i, row in csvreader.iterrows():
-            print(i[0], row['AdjClose'])
-
-
-
-# def analyze1():
-#     with open('analyze.csv', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(["MovingAverage"])
-#         with open('AMZN.csv', newline='') as file1:
-#             ma = 0
-#             csvreader = csv.DictReader(file1):
-#             for row in csvreader:
-#                 ma += row
-#
+        for row in csv.reader(file):
+            ma += int(float(row[1]))
+        print(ma)   
 
 # def read():
 #     data = pd.read_csv('AMZN.csv')
